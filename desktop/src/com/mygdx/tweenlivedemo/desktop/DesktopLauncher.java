62dde9b2cc61ffcaf277a195d29b5f7d2f691f0e
Kyle Piddington <kpiddy@gmail.com>
2015-10-29 16:26:06 -0700

    Pushing starting project to Github, added a few assets to toy with

---
+++ desktop/src/com/mygdx/tweenlivedemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.tweenlivedemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.tweenlivedemo.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
